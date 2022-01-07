d415e9845f8dcfb38a8431daf6b54f9e69fef19e
Alexey Gurtovenko <66733152+AlexeyGurtovenko@users.noreply.github.com>
2022-01-07 13:58:55 +0700

    added subscriber component

--- src/shared/shared.module.ts
+++ src/shared/shared.module.ts
@@ -1,9 +1,16 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { SubscriberComponent } from './subscriber/subscriber.component';
+
+const COMPONENTS = [
+  SubscriberComponent
+];
 
 @NgModule({
-  declarations: [],
+  declarations: [...COMPONENTS],
   imports: [
+    CommonModule
   ],
-  exports: []
+  exports: [...COMPONENTS]
 })
 export class SharedModule { }
